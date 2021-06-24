3ec0f6373fe5be932328521ac0304872a9cd0a8f
Isaac Gonzalez <isaac_gonzalez@outlook.com>
2021-06-23 18:21:44 -0700

    Fin de la clae

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
 var app = new Vue({
     el: "#app",
     data: {
-        mensaje: "Hola Mundo!"
+        mensaje: "Hola Mundo!",
+        visto: true,
+        deshabilitado: true,
+        compras:  [
+            { nombre: "Laptop", precio: 999.99, stock: 10 },
+            { nombre: "Monitor", precio: 765.99, stock: 2 },
+            { nombre: "Mouse", precio: 12.99, stock: 4 },
+            { nombre: "Teclado", precio: 40.99, stock: 5 },
+        ],
+        url: "#top"
+    },
+    methods:  {
+        aumentarStock: function(args) {
+            console.log("Aumentar Stock!");
+            console.log(args);
+        },
+        invertirMensaje: function() { 
+            this.mensaje = this.mensaje.split('').reverse().join('')
+        },
+        cargarStock: function() {
+            stock = this.$http.get("https://gist.githubusercontent.com/IsaacGonzalez/c583809c34ad52b1ebdc516ffccacc43/raw/b78e3f009cf0dc37c129b823bfd814d141ba4470/compras.json")
+            console.log(stock)
+
+        }
+    },
+    computed: {
+        fecha: function(){
+            return Date.now()
+        },
+        articulos_count: function(){
+            return this.compras.length;
+        }
     }
 })
+
---
+++ components/articulo.js
@@ -0,0 +1,4 @@
+Vue.component('articulo', {
+    props: ['articulo'],
+    template: '<li> {{ articulo.nombre }} </li>'
+})
